75d8e5a8d05eb3cca7d6d3939e6ec82dd774ea8c
Ghazi Abbassi <ghaziabbassi1@gmail.com>
2022-06-24 22:26:21 +0100

    remove imports in `drf_standardized_errors.__init__.py` to avoid AppRegistryNotReady in certain situations (fixes #7)

--- drf_standardized_errors/__init__.py
+++ drf_standardized_errors/__init__.py
@@ -1,7 +1,3 @@
 """Standardize your API error responses."""
 
-from .formatter import ExceptionFormatter
-from .handler import ExceptionHandler, exception_handler
-
-__all__ = ["exception_handler", "ExceptionHandler", "ExceptionFormatter"]
 __version__ = "0.10.2"
