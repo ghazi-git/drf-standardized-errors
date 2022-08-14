7a3d2bcd83074d5013dbad8bcf6624c9a06d2407
Ghazi Abbassi <ghaziabbassi1@gmail.com>
2022-08-14 15:21:19 +0100

    create a copy of APISettings to add support for settings whose values are dicts and the values of the dict is a string that represents the path to a class to be imported

--- drf_standardized_errors/settings.py
+++ drf_standardized_errors/settings.py
@@ -3,20 +3,61 @@ from typing import Dict
 from django.conf import settings
 from django.core.signals import setting_changed
 from django.dispatch import receiver
-from rest_framework.settings import APISettings
+from rest_framework.settings import import_from_string, perform_import
 
 
-class PackageSettings(APISettings):
+class PackageSettings:
     """
-    APISettings is quite the nice class for managing settings, just wish that
-    DRF changes its stance on keeping APISettings internal.
+    Copy of DRF APISettings class with support for importing settings that
+    are dicts with value as a string representing the path to the class
+    to be imported.
     """
+
+    setting_name = "DRF_STANDARDIZED_ERRORS"
+
+    def __init__(self, defaults=None, import_strings=None):
+        self.defaults = defaults or DEFAULTS
+        self.import_strings = import_strings or IMPORT_STRINGS
+        self._cached_attrs = set()
 
     @property
     def user_settings(self):
         if not hasattr(self, "_user_settings"):
-            self._user_settings = getattr(settings, "DRF_STANDARDIZED_ERRORS", {})
+            self._user_settings = getattr(settings, self.setting_name, {})
         return self._user_settings
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError("Invalid API setting: '%s'" % attr)
+
+        try:
+            # Check if present in user settings
+            val = self.user_settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            val = self.defaults[attr]
+
+        # Coerce import strings into classes
+        if attr in self.import_strings:
+            if isinstance(val, dict):
+                val = {
+                    status_code: import_from_string(error_schema, attr)
+                    for status_code, error_schema in val.items()
+                }
+            else:
+                val = perform_import(val, attr)
+
+        # Cache the result
+        self._cached_attrs.add(attr)
+        setattr(self, attr, val)
+        return val
+
+    def reload(self):
+        for attr in self._cached_attrs:
+            delattr(self, attr)
+        self._cached_attrs.clear()
+        if hasattr(self, "_user_settings"):
+            delattr(self, "_user_settings")
 
 
 DEFAULTS: Dict = {
@@ -61,11 +102,11 @@ DEFAULTS: Dict = {
 
 IMPORT_STRINGS = ("EXCEPTION_FORMATTER_CLASS", "EXCEPTION_HANDLER_CLASS")
 
-package_settings = PackageSettings(None, DEFAULTS, IMPORT_STRINGS)
+package_settings = PackageSettings(DEFAULTS, IMPORT_STRINGS)
 
 
 @receiver(setting_changed)
 def reload_package_settings(*args, **kwargs):
     setting = kwargs["setting"]
-    if setting == "DRF_STANDARDIZED_ERRORS":
+    if setting == package_settings.setting_name:
         package_settings.reload()
