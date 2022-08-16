48e27ad99fdf8c49f46546bca144a86a69a4626a
Ghazi Abbassi <ghaziabbassi1@gmail.com>
2022-08-16 20:25:54 +0100

    move the comments for schema generation settings to the settings docs page

--- drf_standardized_errors/settings.py
+++ drf_standardized_errors/settings.py
@@ -76,26 +76,9 @@ DEFAULTS: Dict = {
         "429",
         "500",
     ],
-    # A mapping used to override the default serializers used to describe
-    # the error response. The key is the status code and the value is a string
-    # that represents the path to the serializer class that describes the
-    # error response.
     "ERROR_SCHEMAS": None,
-    # When there is a validation error in list serializers, the "attr" returned
-    # will be sth like "0.email", "1.email", "2.email", ... So, to describe
-    # the error codes linked to the same field in a list serializer, the field
-    # will appear in the schema with the name "INDEX.email"
     "LIST_INDEX_IN_API_SCHEMA": "INDEX",
-    # When there is a validation error in a DictField with the name "extra_data",
-    # the "attr" returned will be sth like "extra_data.<key1>", "extra_data.<key2>",
-    # "extra_data.<key3>", ... Since the keys of a DictField are not predetermined,
-    # this setting is used as a common name to be used in the API schema. So, the
-    # corresponding "attr" value for the previous example will be "extra_data.KEY"
     "DICT_KEY_IN_API_SCHEMA": "KEY",
-    # should be unique to error components since it is used to identify error
-    # components generated dynamically to exclude them from being processed by
-    # the postprocessing hook. This avoids raising warnings for "code" and "attr"
-    # which can have the same choices across multiple serializers.
     "ERROR_COMPONENT_NAME_SUFFIX": "ErrorComponent",
 }
 
