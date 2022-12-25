f08094632203594e2004b15d032a79a99f238187
ghazi <ghaziabbassi1@gmail.com>
2022-12-25 23:29:20 +0100

    add tests for the @extend_validation_errors decorator

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from drf_spectacular.generators import SchemaGenerator
 from drf_spectacular.validation import validate_schema
 
@@ -12,5 +12,19 @@ def generate_view_schema(route, view):
     return schema
 
 
+def generate_versioned_view_schema(view, version):
+    patterns = [re_path(r"^(?P<version>(v1|v2|v3))/validate/", view)]
+    generator = SchemaGenerator(patterns=patterns, api_version=version)
+    schema = generator.get_schema(request=None, public=True)
+    validate_schema(schema)
+    return schema
+
+
 def get_responses(schema: dict, route: str, method="get"):
     return schema["paths"][f"/{route}"][method]["responses"]
+
+
+def get_error_codes(api_schema, schema_name):
+    return api_schema["components"]["schemas"][schema_name]["properties"]["code"][
+        "enum"
+    ]
