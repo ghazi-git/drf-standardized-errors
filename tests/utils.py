1a0001157d56b3f7144880626d7b7cee43d94071
ghazi <ghaziabbassi1@gmail.com>
2022-12-25 14:04:55 +0100

    remove unused variable and move imports outside the generate_view_schema function

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,9 @@
+from django.urls import path
+from drf_spectacular.generators import SchemaGenerator
 from drf_spectacular.validation import validate_schema
 
 
 def generate_view_schema(route, view):
-    from django.urls import path
-    from drf_spectacular.generators import SchemaGenerator
-
     patterns = [path(route, view)]
 
     generator = SchemaGenerator(patterns=patterns)
@@ -15,22 +14,3 @@ def generate_view_schema(route, view):
 
 def get_responses(schema: dict, route: str, method="get"):
     return schema["paths"][f"/{route}"][method]["responses"]
-
-
-d = {
-    "openapi": "3.0.3",
-    "info": {"title": "", "version": "0.0.0"},
-    "paths": {
-        "/auth/": {
-            "get": {
-                "operationId": "auth_retrieve",
-                "tags": ["auth"],
-                "security": [{"basicAuth": []}],
-                "responses": {"200": {"description": "No response body"}},
-            }
-        }
-    },
-    "components": {
-        "securitySchemes": {"basicAuth": {"type": "http", "scheme": "basic"}}
-    },
-}
