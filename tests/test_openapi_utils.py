dbc37d4228bdefa858ab299517097d6e52a0b698
Kerstin Humm <kerstin@erictapen.name>
2025-02-15 15:47:34 +0100

    fix test_openapi_utils

--- tests/test_openapi_utils.py
+++ tests/test_openapi_utils.py
@@ -2,6 +2,7 @@ import sys
 from unittest import mock
 
 import pytest
+import django
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
@@ -515,7 +516,10 @@ def test_char_fields_with_error_codes():
     assert slug.error_codes == {"invalid", "null_characters_not_allowed"}
     assert regex.error_codes == {"invalid", "required", "null_characters_not_allowed"}
     assert uuid.error_codes == {"invalid", "required", "null_characters_not_allowed"}
-    assert ip.error_codes == {"invalid", "null_characters_not_allowed"}
+    if django.VERSION >= (4, 2):
+        assert ip.error_codes == {"invalid", "null_characters_not_allowed", "max_length"}
+    else:
+        assert ip.error_codes == {"invalid", "null_characters_not_allowed"}
 
 
 class NumberForm(forms.Form):
